ffc09eeed1c3b840ea4a24b15cffbb29d5156c4b
DevopsCube <admin@comtechies.com>
2019-06-02 13:24:15 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,6 +3,6 @@ def call(Map stageParams = [:]) {
     checkout([
         $class: 'GitSCM',
         branches: [[name: '*/${stageParams.version}']],
-        userRemoteConfigs: [[url: stepParams.url, credentialsId: stepParams.credentialsId ?: null]]
+        userRemoteConfigs: [[url: stageParams.url]]
     ])
   }
