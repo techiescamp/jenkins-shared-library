e57de731bd76bdfdc08fb9d83358a28974711fc0
Aswin1118 <aswinvj390@gmail.com>
2024-09-19 18:23:45 +0530

    update

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -3,7 +3,7 @@ def kaniko() {
         def tplContent = libraryResource "org/dcube/trivy/html.tpl"
         writeFile file: "${WORKSPACE}/html.tpl", text: tplContent
 
-        def trivyConfigContent = libraryResource "trivy/trivy.yml"
+        def trivyConfigContent = libraryResource "org/dcube/trivy/trivy.yml"
         writeFile file: "${WORKSPACE}/trivy.yml", text: trivyConfigContent
 
         def command = "trivy image --config ${WORKSPACE}/trivy.yml --template '@${WORKSPACE}/html.tpl' -o ${WORKSPACE}/trivy-report.html --input ${WORKSPACE}/${BUILD_NUMBER}.tar"
